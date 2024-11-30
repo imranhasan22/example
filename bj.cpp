74c4825e9041a6bef545e55d00a5899ba808b0b9
imranhasan22 <imran258hasan@gmail.com>
2024-11-30 21:07:33 +0600

    fourth

---
+++ bj.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a / gcd(a, b)) * b
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+       
+
+    }
+    return 0;
+}
